0501a43ded8aac11eff59ef6a45665ec34dcb0c8
Aitor MuÃ±oz Escobar <Aitor.escobar@kiwi.com>
2026-03-29 23:40:02 +0200

    Minimal app test

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
- 
+
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Parlora AI',
@@ -19,28 +19,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: '#0D0D14',
     },
     package: 'com.parloraai.app',
-    permissions: [
-      'android.permission.RECORD_AUDIO',
-      'android.permission.BLUETOOTH',
-      'android.permission.BLUETOOTH_ADMIN',
-      'android.permission.BLUETOOTH_CONNECT',
-      'android.permission.BLUETOOTH_SCAN',
-      'android.permission.ACCESS_FINE_LOCATION',
-      'android.permission.INTERNET',
-    ],
-    googleServicesFile: './google-services.json',
   },
-  ios: {
-    bundleIdentifier: 'com.parloraai.app',
-    googleServicesFile: './GoogleService-Info.plist',
-  },
-  plugins: [
-    'expo-secure-store',
-  ],
   extra: {
-    DEEPL_PROXY_URL: 'https://parlora-backend.up.railway.app',
-    GOOGLE_WEB_CLIENT_ID: '1085428374188-uunn9cgkk2t9oai9u0b5buuuqcdkvhbr.apps.googleusercontent.com',
-    GOOGLE_ANDROID_CLIENT_ID: '1085428374188-uunn9cgkk2t9oai9u0b5buuuqcdkvhbr.apps.googleusercontent.com',
     eas: {
       projectId: '03307d0c-78f5-4ddb-9088-c06470c182a1',
     },
